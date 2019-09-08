1c9be243950d87d9886d96d4da8c6df353e5e442
Jordan Rust <jordan.rust92@gmail.com>
2019-09-08 10:56:06 -0400

    Adding text editor

--- src/organisms/editPage.js
+++ src/organisms/editPage.js
@@ -2,15 +2,16 @@ import React from 'react';
 import VerticalDivider from '../atoms/verticalDivider';
 
 import "./editPage.scss";
+import TextEditor from '../atoms/textEditor';
 
 const EditPage = () => {
   return (
     <div className="editPage__block">
-        <div>
-
-        </div>
+        <TextEditor 
+          placeholder="Tell your story..."
+        />
         <VerticalDivider />
-        <div>
+        <div style={{ width: '100%' }}>
 
         </div>
     </div>
